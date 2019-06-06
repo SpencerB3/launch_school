050bd57d5c9397d835eff23e2a14328eec218e52
Spencer Brainard <spencer.brainard@gmail.com>
2019-06-06 15:21:19 -0700

    Initializing repository

---
+++ lesson_2/pseudocode_practice.rb
@@ -0,0 +1,40 @@
+# 1 - a method that returns the sum of two integers
+# 2 - a method that takes an array of strings, and returns a string that is all those strings concatenated together
+# 3 - a method that takes an array of integers, and returns a new array with every other element
+
+# 1.
+# START
+
+# SET a method with two parameters
+#   RETURN the sum of two numbers
+# END METHOD
+# PRINT method with two numerical arguments
+# END
+
+# 2.
+# START
+
+# SET an array of strings
+# SET an empty string
+# ITERATE though array
+#   ADD each string fromn array to the empty string
+# END ITERATION
+# PRINT formerly empty string
+
+# END
+
+# 3.
+# START
+
+# SET an array of numbers
+# SET an empty array
+# ITERATE through array containing numbers --or use map method
+#   IF index is odd
+#     PUSH number into the empty array
+#   END IF statement
+# END
+
+
+
+
+
