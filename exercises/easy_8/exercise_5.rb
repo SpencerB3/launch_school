51c16c4fb76dfaf0a2e7aa2297f329835120634e
Spencer Brainard <spencer.brainard@gmail.com>
2019-10-22 18:15:08 -0400

    Account check

--- exercises/easy_8/exercise_5.rb
+++ exercises/easy_8/exercise_5.rb
@@ -1,38 +1,39 @@
 # Write a method that returns a list of all substrings of a string that are palindromic. That is, each substring must consist of the same sequence of characters forwards as it does backwards. The return value should be arranged in the same sequence as the substrings appear in the string. Duplicate palindromes should be included multiple times.
 
-def palindromes(string)
-
-  all_substrings = substrings(string)
+def substrings_at_start(string)
+  new_string = ''
   result = []
-  all_substrings.each do |substring|
-    results << substring if is_palindrome?(string)
+  string.each_char do |char|
+    new_string += char
+    result << new_string
   end
   result
 end
 
-def is_palindrome?(string)
-  string == string.reverse && string.size > 1
-end
-
-def substrings_at_start(string)
-  accumulated_array = []
-  new_string = ''
-  string.each_char do |char|
-    new_string += char
-    accumulated_array << new_string
-  end
-  accumulated_array
-end
-
 def substrings(string)
   result = []
-  i = 0
   loop do
     result.concat(substrings_at_start(string))
     string = string[1..-1]
     break if string.size == 0
   end
+   result
+end
+
+def palindromes(string)
+
+  result = []
+  all_substring = substrings(string)
+  all_substring.each do |substring|
+    if is_palindrome?(substring)
+      result << substring
+    end
+  end
   result
+end
+
+def is_palindrome?(substring)
+  substring == substring.reverse && substring.size > 1
 end
 
 palindromes('abcd') == []
