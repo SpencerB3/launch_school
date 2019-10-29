31aa672dbbafb099016521efd431f48d3dc4bf44
Spencer Brainard <spencer.brainard@gmail.com>
2019-10-29 11:49:34 -0400

    add new version ex. 11.2

--- exercises/medium_1/exercise_2.rb
+++ exercises/medium_1/exercise_2.rb
@@ -1,18 +1,13 @@
 # Write a method that can rotate the last n digits of a number. For example:
 
-
 def rotate_rightmost_digits(number, n)
-
-  number_array = number.to_s.split('')
-  first_half = number_array[0...-n]
-  second_half = number_array[-n..n]
-
-  (first_half + rotate_array(second_half)).join.to_i
+  number_array = number.to_s.chars
+  number_array[-n..-1] = rotate_array(number_array[-n..-1])
+  number_array.join.to_i
 end
 
 def rotate_array(array)
-  array.empty? ? array : array[1..-1] + array[0]
-  # array.empty? ? array : array.push(array.shift)
+  array.empty? ? array : array[1..-1] << array[0]
 end
 
 rotate_rightmost_digits(735291, 1) == 735291
