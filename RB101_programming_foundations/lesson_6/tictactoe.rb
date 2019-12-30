5bd3ed178e8b18cf63b0c40a14bd3422105a0a37
Spencer Brainard <spencer.brainard@gmail.com>
2019-12-30 13:01:04 -0800

    Refactor tictactoe

--- RB101_programming_foundations/lesson_6/tictactoe.rb
+++ RB101_programming_foundations/lesson_6/tictactoe.rb
@@ -1,11 +1,10 @@
-require 'pry'
-
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
                 [[1, 5, 9], [3, 5, 7]]
+NUMBER_OF_WINS = 5
 
 def prompt(message)
   puts "=> #{message}"
@@ -41,10 +40,21 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def joinor(arr, delimiter = ', ', word= 'or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt("Choose a square (#{empty_squares(brd).join(', ')}):")
+    prompt("Choose a square #{joinor(empty_squares(brd))}")
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt("Sorry, that's not a valid choice")
@@ -77,30 +87,60 @@ def detect_winner(brd)
   nil
 end
 
+def display_score(score1, score2)
+  prompt("Player Wins: #{score1} - Computer Wins: #{score2}")
+end
+
+def match_ended?(score1, score2)
+  score1 == NUMBER_OF_WINS || score2 == NUMBER_OF_WINS
+end
+
+def announce_winner(score)
+  if score == NUMBER_OF_WINS
+    prompt("You won best of five -- Congrats!")
+  else
+    prompt("The computer has won!")
+  end
+end
+
+# main body
 loop do
-  board = initialize_board
+  prompt('Welcome to TicTacToe')
+  prompt("First player to win #{NUMBER_OF_WINS} times wins the tournament")
+  player_wins = 0
+  computer_wins = 0
 
   loop do
+    board = initialize_board
+
+    loop do
+      display_board(board)
+      player_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+
+      computer_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+    end
+
     display_board(board)
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    if someone_won?(board)
+      prompt "#{detect_winner(board)} won!"
+      detect_winner(board) == 'Player' ? player_wins += 1 : computer_wins += 1
+      display_score(player_wins, computer_wins)
+    else
+      prompt "It's a tie!"
+    end
 
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    break if match_ended?(player_wins, computer_wins)
+
+    sleep(2)
   end
+  announce_winner(player_wins)
 
-  display_board(board)
-
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
-
-  prompt("Do you want to play again? (Type y or n)")
-  answer = gets.chomp.downcase
-  break unless answer.start_with?('y')
+  prompt("Do you want to play again? (y or n)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
 prompt("Thanks for playing Tic Tac Toe! Good bye!")
