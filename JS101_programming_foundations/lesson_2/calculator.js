0516d33ed354e0db37e69c03b7df65719d8dca91
Spencer Brainard <spencer.brainard@gmail.com>
2020-01-09 13:20:07 -0800

    Rebegin Calculator assignment

--- JS101_programming_foundations/lesson_2/calculator.js
+++ JS101_programming_foundations/lesson_2/calculator.js
@@ -1,81 +1,62 @@
-const messagesJSON = require('calculator_messages.json');
-const MESSAGES = JSON.parse(messagesJSON);
+const MESSAGES = require('./calculator_messages.json');
 
 const rlSync = require('readline-sync');
 
-const log = (message) => console.log(`=> ${message}`);
-let anotherCalc;
-let name = rlSync.prompt();
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
 function invalidNumber(number) {
-  return number.trimStart() === '' || Number.isNaN(Number(number));
+  return Number.isNaN(number);
 }
+let answer;
 
-function isValidName(name) {
-  return name.trimStart() !== '';
-}
-
-while (!isValidName(name)) {
-  console.log('Hmmm... that does not seem like you typed anything.  Try again.');
-  name = rlSync.prompt();
-}
-
-log(MESSAGES['welcome']);
-log(MESSAGES['name']);
-
-log(`Hi, ${name}!`);
+// main code
+prompt('Welcome to Calculator!');
 
 do {
-log('Enter your first number:');
-let number1 = rlSync.question();
+  let number1 = Number(rlSync.question("=> What's your first number?\n"));
 
-while (invalidNumber(number1)) {
-  log('Hmmm... that does not look like a valid number. Try again');
-  number1 = rlSync.question();
-}
+  while (invalidNumber(number1)) {
+    prompt("Hmm, that doesn't look like a valid number.  Try again");
+    number1 = rlSync.question();
+  }
 
-log('Enter your second number:');
-let number2 = rlSync.question();
+  let number2 = Number(rlSync.question("=> What's your second number?\n"));
 
-while (invalidNumber(number2)) {
-  log('Hmmm... that does not look like a valid number. Try again');
-  number2 = rlSync.question();
-}
+  while (invalidNumber(number2)) {
+    prompt("Hmm, that doesn't look like a valid number.  Try again");
+    number2 = rlSync.question();
+  }
 
-log("What operation would you like to perform?\n" +
-           "1) Add 2) Subtract 3) Multiply 4) Divide");
-let operation = rlSync.question();
+  prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
+  let operation = Number(rlSync.question());
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  log('Hmmm... that does not look like a valid option.  Try again');
-  operation = rlSync.question();
-}
+  while (![1, 2, 3, 4].includes(operation)) {
+    prompt('Must choose 1, 2, 3, or 4');
+    operation = rlSync.questionFloat();
+  }
 
-let output;
+  let output;
+  switch (operation) {
+    case 1:
+      output = number1 + number2;
+      break;
+    case 2:
+      output = number1 - number2;
+      break;
+    case 3:
+      output = number1 * number2;
+      break;
+    case 4:
+      output = number1 / number2;
+  }
 
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
+  prompt(`The result is ${output}.`);
 
-log(`The result is ${output}.`);
+  prompt("Would you like to do another calculation? Type y or n")
+  answer = rlSync.question().toLowerCase;
 
-log('Would you like to do another calculation?')
-log("Enter 'y' if yes, else enter any other key to exit this program")
+} while (answer === 'y');
 
-anotherCalc = rlSync.prompt().toLowerCase();
-console.clear();
-
-} while (anotherCalc === 'y');
-
-log('Thank you for using Calculator!');
+prompt('Thank you for using Calculator!  Good bye!');
--- JS101_programming_foundations/lesson_2/debug.js
+++
@@ -1,7 +0,0 @@
-let counter = 1;
-
-while (counter <= 5) {
-  console.log(counter);
-  debugger;
-  counter += 1;
-}
