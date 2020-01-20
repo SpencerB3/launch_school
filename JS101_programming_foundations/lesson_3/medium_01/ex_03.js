c834579d43b34d4c6fd7ff91d6d52d0fef6e91cf
Spencer Brainard <spencer.brainard@gmail.com>
2020-01-20 11:18:16 -0800

    Add the remaining  Medium_1 files

---
+++ JS101_programming_foundations/lesson_3/medium_01/ex_03.js
@@ -0,0 +1,31 @@
+// Alan wrote the following function, which was intended to return all of the factors of number:
+
+// function factors(number) {
+//   let divisor = number;
+//   let factors = [];
+//   do {
+//     if (number % divisor === 0) {
+//       factors.push(number / divisor);
+//     }
+//     divisor -= 1;
+//   } while (divisor !== 0);
+//   return factors;
+// }
+
+// Alyssa noticed that this code would fail when the input is 0 or a negative number, and asked Alan to change the loop.How can he make this work without using a do /while loop? Note that we're not looking to find the factors for 0 or negative numbers, but we want to handle it gracefully instead of raising an exception or going into an infinite loop.
+
+// Bonus: What is the purpose of number % divisor === 0 in that code ?
+
+function factors(number) {
+  let divisor = number;
+  let factors = [];
+  while (divisor > 0) {
+    if (number % divisor === 0) {
+      factors.push(number / divisor);
+    }
+    divisor -= 1;
+  }
+  return factors;
+}
+
+// The purpose of 'number % divisor === 0' is to make sure the number has no remainder is therefore an integer.
