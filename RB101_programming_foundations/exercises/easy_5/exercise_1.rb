4ac4bd3b0013d1cd0f0a51c04a6f1cdc9bb3dcb4
Spencer Brainard <spencer.brainard@gmail.com>
2021-09-26 20:13:42 -0400

    revise exercise 10

--- RB101_programming_foundations/exercises/easy_5/exercise_1.rb
+++ RB101_programming_foundations/exercises/easy_5/exercise_1.rb
@@ -1,8 +1,12 @@
-# Write a method that determines and returns the ASCII string value of a string that is passed in as an argument. The ASCII string value is the sum of the ASCII values of every character in the string.
-
 def ascii_value(string)
+
   value = 0
-  string.each_char { |char| value += char.ord }
+
+  string.each_char do |char|
+
+    value += char.ord
+
+  end
   value
 end
 
