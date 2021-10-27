71dfe2f9ca900131489f7ce93a810637cd125d83
Spencer Brainard <spencer.brainard@gmail.com>
2021-10-27 12:04:00 -0400

    Adding inheritance exercises

---
+++ RB120_object_oriented_programming/oop_exercises/inheritance_03/ex_08.rb
@@ -0,0 +1,15 @@
+# Using the following code, determine the lookup path used when invoking cat1.color. Only list the classes and modules that Ruby will check when searching for the #color method.
+
+class Animal
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new
+cat1.color
+
+# answer => Cat, Animal, Kernel, Object
---
+++ RB120_object_oriented_programming/oop_exercises/inheritance_03/ex_10.rb
@@ -0,0 +1,11 @@
+# Create a module named Transportation that contains three classes: Vehicle, Truck, and Car. Truck and Car should both inherit from Vehicle.
+
+module Transportation
+  class Vehicle; end
+
+  class Truck < Vehicle
+  end
+
+  class Car < Vehicle
+  end
+end
