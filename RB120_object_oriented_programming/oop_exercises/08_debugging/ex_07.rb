f2fbfe36860fe6d71f36013f3181b3d8d10e7a01
Spencer Brainard <spencer.brainard@gmail.com>
2021-11-09 12:11:28 -0500

    Adding debugging exercises 6 to 10

---
+++ RB120_object_oriented_programming/oop_exercises/08_debugging/ex_07.rb
@@ -0,0 +1,55 @@
+# We created a simple BankAccount class with overdraft protection, that does not allow a withdrawal greater than the amount of the current balance. We wrote some example code to test our program. However, we are surprised by what we see when we test its behavior. Why are we seeing this unexpected output? Make changes to the code so that we see the appropriate behavior.
+
+class BankAccount
+  attr_reader :balance
+
+  def initialize(account_number, client)
+    @account_number = account_number
+    @client = client
+    @balance = 0
+  end
+
+  def deposit(amount)
+    if amount > 0
+      self.balance += amount
+      "$#{amount} deposited. Total balance is $#{balance}."
+    else
+      "Invalid. Enter a positive amount."
+    end
+  end
+
+  def withdraw(amount)
+    if balance > 0 && valid_transaction?(balance - amount)
+      self.balance -= amount
+      success = (self.balance -= amount)
+    else
+      success = false
+    end
+
+    if success
+      "$#{amount} withdrawn. Total balance is $#{balance}."
+    else
+      "Invalid. Enter positive amount less than or equal to current balance ($#{balance})."
+    end
+  end
+
+  def balance=(new_balance)
+    @balance = new_balance
+  end
+
+  def valid_transaction?(new_balance)
+    new_balance >= 0
+  end
+end
+
+# Example
+
+account = BankAccount.new('5538898', 'Genevieve')
+
+                          # Expected output:
+p account.balance         # => 0
+p account.deposit(50)     # => $50 deposited. Total balance is $50.
+p account.balance         # => 50
+p account.withdraw(80)    # => Invalid. Enter positive amount less than or equal to current balance ($50).
+                          # Actual output: $80 withdrawn. Total balance is $50.
+p account.balance         # => 50
---
+++ RB120_object_oriented_programming/oop_exercises/08_debugging/ex_10.rb
@@ -0,0 +1,61 @@
+# We discovered Gary Bernhardt's repository for finding out whether something rocks or not, and decided to adapt it for a simple example.
+
+class AuthenticationError < StandardError; end
+
+# A mock search engine
+# that returns a random number instead of an actual count.
+class SearchEngine
+  def self.count(query, api_key)
+    unless valid?(api_key)
+      raise AuthenticationError, 'API key is not valid.'
+    end
+
+    rand(200_000)
+  end
+
+  private
+
+  def self.valid?(key)
+    key == 'LS1A'
+  end
+end
+
+module DoesItRock
+  API_KEY = 'LS1A'
+
+  class NoScore; end
+
+  class Score
+    def self.for_term(term)
+      positive = SearchEngine.count(%{"#{term} rocks"}, API_KEY).to_f
+      negative = SearchEngine.count(%{"#{term} is not fun"}, API_KEY).to_f
+
+      positive / (positive + negative)
+    rescue ZeroDivisionError
+      NoScore.new
+    end
+  end
+
+  def self.find_out(term)
+    score = Score.for_term(term)
+
+    case score
+    when NoScore
+      "No idea about #{term}..."
+    when 0...0.5
+      "#{term} is not fun."
+    when 0.5
+      "#{term} seems to be ok..."
+    else
+      "#{term} rocks!"
+    end
+  rescue StandardError => e
+    e.message
+  end
+end
+
+# Example (your output may differ)
+
+puts DoesItRock.find_out('Sushi')       # Sushi seems to be ok...
+puts DoesItRock.find_out('Rain')        # Rain is not fun.
+puts DoesItRock.find_out('Bug hunting') # Bug hunting rocks!
