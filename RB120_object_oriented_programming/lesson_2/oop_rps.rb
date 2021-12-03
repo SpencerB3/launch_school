72db8bce95ea7bace8dedaccf29d807caa872c2b
Spencer Brainard <spencer.brainard@gmail.com>
2021-12-02 20:37:06 -0500

    Adding revisited rsp assignment

--- RB120_object_oriented_programming/lesson_2/oop_rps.rb
+++ RB120_object_oriented_programming/lesson_2/oop_rps.rb
@@ -1,37 +1,3 @@
-class Move
-  VALUES = ['rock', 'paper', 'scissors']
-
-  def initialize(value)
-    @value = value
-  end
-
-  def scissors?
-    @value == 'scissors'
-  end
-
-  def rock?
-    @value == 'rock'
-  end
-
-  def paper?
-    @value == 'paper'
-  end
-
-  def >(other_move)
-    (rock? && other_move.scissors?) || (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?)
-  end
-
-  def <(other_move)
-    (rock? && other_move.paper?) || (paper? && other_move.scissors?) ||
-      (scissors? && other_move.rock?)
-  end
-
-  def to_s
-    @value
-  end
-end
-
 class Player
   attr_accessor :move, :name
 
@@ -41,81 +7,85 @@ class Player
 end
 
 class Human < Player
-  def set_name
-    n = ""
-    loop do
-      puts "What's your name?"
-      n = gets.chomp
-      break unless n.empty?
-      puts 'Sorry, must enter a value.'
-    end
-    self.name = n
-  end
-
   def choose
     choice = nil
     loop do
-      puts 'Please choose rock, paper, or scissors:'
-      choice = gets.chomp
+      print "Please choose rock, paper, or scissors: "
+      choice = gets.chomp.downcase
       break if Move::VALUES.include?(choice)
-      puts "Sorry, invalid choice."
+      puts "Invalid entry - let's try again..."
     end
     self.move = Move.new(choice)
+  end
+
+  def set_name
+    n = ""
+    loop do
+      print "Please enter your name: "
+      n = gets.chomp
+      break unless n.empty?
+      puts " Invalid entry - Let's try again..."
+    end
+    self.name = n
   end
 end
 
 class Computer < Player
-  def set_name
-    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number_5'].sample
-  end
-
   def choose
     self.move = Move.new(Move::VALUES.sample)
   end
+
+  def set_name
+    self.name = ['R2D2', 'Chappie', 'Hal', 'Sonny', 'Number 5'].sample
+  end
 end
 
-# Game Orchestration Engine
+class Move
+  VALUES = %w(rock paper scissors).freeze
+
+  attr_reader :value
+
+  attr_reader :move
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    value == 'scissors'
+  end
+
+  def paper?
+    value == 'paper'
+  end
+
+  def rock?
+    value == 'rock'
+  end
+
+  def >(other)
+    (rock? && other.scissors?) ||
+      (paper? && other.rock?) ||
+      (scissors? && other.paper?)
+  end
+
+  def <(other)
+    (scissors? && other.rock?) ||
+      (rock? && other.paper?) ||
+      (paper? && other.scissors?)
+  end
+
+  def to_s
+    value
+  end
+end
+
 class RPSGame
   attr_accessor :human, :computer
 
   def initialize
     @human = Human.new
     @computer = Computer.new
-  end
-
-  def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors!"
-  end
-
-  def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors.  Good bye!"
-  end
-
-  def display_moves
-    puts "#{human.name} chose #{human.move}."
-    puts "#{computer.name} chose #{computer.move}."
-  end
-
-  def display_winner
-    if human.move > computer.move
-      puts "#{human.name} won!"
-    elsif human.move < computer.move
-      puts "#{computer.name} won!"
-    else
-      puts "It's a tie!"
-    end
-  end
-
-  def play_again?
-    answer = nil
-    loop do
-      puts "Would you like to play again? (y/n)"
-      answer = gets.chomp
-      break if ['y', 'n'].include?(answer.downcase)
-      puts 'Sorry, must be y or n.'
-    end
-
-    answer.downcase == 'y'
   end
 
   def play
@@ -128,6 +98,43 @@ class RPSGame
       break unless play_again?
     end
     display_goodbye_message
+  end
+
+  private
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thank you for playing Rock, Paper, Scissors - Goodbye!"
+  end
+
+  def display_moves
+    puts "#{human.name} chose: #{human.move}"
+    puts "#{computer.name} chose: #{computer.move}"
+  end
+
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif computer.move > human.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp.downcase
+      break if %w(y yes n no).include?(answer)
+      puts "Please only type 'y' or 'n'/  Let's try again..."
+    end
+
+    answer == 'y' || answer == 'yes'
   end
 end
 
