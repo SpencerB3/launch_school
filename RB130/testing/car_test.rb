08f331c06839ee1d28c5c6816f50bd0a73ab1200
Spencer Brainard <spencer.brainard@gmail.com>
2022-01-18 17:35:47 -0800

    Update minitest demonstrations

--- RB130/testing/car_test.rb
+++ RB130/testing/car_test.rb
@@ -1,18 +1,59 @@
+# require 'minitest/autorun'
+# require "minitest/reporters"
+# Minitest::Reporters.use!
+
+# require_relative 'car'
+
+# class CarTest < MiniTest::Test
+#   def test_wheels
+#     car = Car.new
+#     assert_equal(4, car.wheels)
+#   end
+
+#   def test_bad_wheels
+#     skip
+#     car = Car.new
+#     assert_equal(3, car.wheels)
+#   end
+# end
+
 require 'minitest/autorun'
-require "minitest/reporters"
-Minitest::Reporters.use!
 
 require_relative 'car'
 
 class CarTest < MiniTest::Test
-  def test_wheels
-    car = Car.new
-    assert_equal(4, car.wheels)
+
+  def setup
+    @car = Car.new
   end
 
-  def test_bad_wheels
-    skip
+  def test_car_exists
+    assert(@car)
+  end
+
+  def test_wheels
+    assert_equal(4, @car.wheels)
+  end
+
+  def test_name_is_nil
+    assert_nil(@car.name)
+  end
+
+  def test_raise_initialize_with_arg
+    assert_raises(ArgumentError) do
+      Car.new(name: "Joey")
+    end
+  end
+
+  def test_instance_of_car
     car = Car.new
-    assert_equal(3, car.wheels)
+    assert_instance_of(Car, @car)
+  end
+
+  def test_includes_car
+    arr = [1, 2, 3]
+    arr << @car
+
+    assert_includes(arr, @car)
   end
 end
