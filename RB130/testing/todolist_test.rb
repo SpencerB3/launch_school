a4538c7b7ef23b214ef565971e2db0fff8f12671
Spencer Brainard <spencer.brainard@gmail.com>
2022-01-20 20:20:49 -0800

    Adding todolist_test assignment

---
+++ RB130/testing/todolist_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+require_relative 'todolist'
+
+class TodoListTest < MiniTest::Test
+
+  def setup
+    @todo1 = Todo.new("Buy milk")
+    @todo2 = Todo.new("Clean room")
+    @todo3 = Todo.new("Go to gym")
+    @todos = [@todo1, @todo2, @todo3]
+
+    @list = TodoList.new("Today's Todos")
+    @list.add(@todo1)
+    @list.add(@todo2)
+    @list.add(@todo3)
+  end
+
+  # Your tests go here. Remember they must start with "test_"
+
+end
