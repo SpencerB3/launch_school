0946e23c388c2637dd55c48ecb3801eabc14d979
Spencer Brainard <spencer.brainard@gmail.com>
2022-12-09 13:04:27 -0500

    Update sum_of_multiples challenge

--- RB130/ruby_challenges/sum_of_multiples.rb
+++ RB130/ruby_challenges/sum_of_multiples.rb
@@ -1,25 +1,50 @@
+# My first solution commented out below.
+
 class SumOfMultiples
   attr_reader :multiples
 
-  def self.to(number)
-    SumOfMultiples.new.to(number)
+  def self.to(num)
+    SumOfMultiples.new().to(num)
   end
 
   def initialize(*multiples)
-    @multiples = (multiples.empty?) ? [3, 5] : multiples
+    @multiples = !multiples.empty? ? multiples : [3, 5]
   end
 
-  def to(number)
-    (1...number).select do |num|
-      any_multiples?(num)
+  def to(num)
+    (1...num).select do |n|
+      any_multiples?(n)
     end.sum
   end
 
   private
 
-  def any_multiples?(num)
-    multiples.any? do |n|
-      (num % n).zero?
-    end
+  def any_multiples?(n)
+    multiples.any? { |multiple| (n % multiple).zero? }
   end
 end
+
+# class SumOfMultiples
+#   def initialize(*multiples)
+#     @multiples = multiples
+#   end
+
+#   def to(num)
+#     self.class.to(num, @multiples)
+#   end
+
+#   def self.to(num, multiples = [3, 5])
+#     sum = 0
+
+#     1.upto(num - 1) do |n|
+#       multiples.each do |multiple|
+#         if (n % multiple).zero?
+#           sum += n
+#           break
+#         end
+#       end
+#     end
+
+#     sum
+#   end
+# end
